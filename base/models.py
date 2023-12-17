202d0ddb19c54c695e33472bfb0247f7e364e20f
vladislavi_18 <ilcvlad922@gmail.com>
2023-12-17 18:44:37 +0300

    добавил дату

--- base/models.py
+++ base/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(null=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
--- base/views.py
+++ base/views.py
@@ -62,7 +62,7 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title', 'description', 'complete']
+    fields = ['title', 'description', 'date', 'complete']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
@@ -72,7 +72,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['title', 'description', 'complete']
+    fields = ['title', 'description', 'date', 'complete']
     success_url = reverse_lazy('tasks')
 
 
